78063ac646ef1be0bd7fdf351143e7ba4a7cbbc0
Michael Arntzenius <daekharel@gmail.com>
2010-09-08 11:28:29 -0400

    AST rewrite & rename

---
+++ Text/Markup/AST.hs
@@ -0,0 +1,8 @@
+module Text.Markup.AST ( Elem (..), Content (..) )
+where
+
+data Elem = Elem { elemTag :: String, elemContent :: [Content] }
+            deriving (Show, Eq, Ord)
+
+data Content = Text String | Child Elem
+               deriving (Show, Eq, Ord)
--- Text/Markup/Ast.hs
+++
@@ -1,6 +0,0 @@
-module Text.Markup.Ast ( Markup (..) )
-where
-
-data Markup = Element { eltTag :: String, eltChildren :: [Markup] }
-            | Text String
-              deriving (Show, Eq, Ord)
